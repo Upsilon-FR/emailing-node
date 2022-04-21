337d052bc2b2708a05b90cb8605566623b5cd342
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2022-04-21 21:40:02 +0200

    Endpoint et http statistiques finis

--- server/statistiques/stats.controller.ts
+++ server/statistiques/stats.controller.ts
@@ -2,16 +2,54 @@ import { Request, Response } from "express";
 import StatsModel from "./stats.model";
 
 export default class StatsCtrl {
-  static fnct = (req: Request, res: Response) => {};
   // TODO: implement controller
 
-  //total utilisateurs dont x admin et y user
-  static ttlUsers = (req: Request, res: Response) => {};
+  /**
+   * Nombre total utilisateurs dont x admin et y user
+   *
+   * @param req
+   * @param res
+   */
+  static ttlUsers = (req: Request, res: Response) => {
+    let ttlUsersQuery = new StatsModel().queryNbUsers();
+    //Requête SQL
+    ttlUsersQuery
+      .then((response) => {
+        console.log(response);
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).send(error);
+      });
+  };
 
-  //nombre total de contact
-  static ttlContact = (req: Request, res: Response) => {};
+  /**
+   * Nombre total de contact
+   *
+   * @param req
+   * @param res
+   */
+  static ttlContact = (req: Request, res: Response) => {
+    let ttlContactQuery = new StatsModel().queryNbContact();
+    //Requête SQL
+    ttlContactQuery
+      .then((response) => {
+        console.log(response);
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).send(error);
+      });
+  };
 
-  //nombre total de list
+  /**
+   * Nombre total de list
+   *
+   * @param req
+   * @param res
+   */
   static ttlList = (req: Request, res: Response) => {
     let nbListQuery = new StatsModel().queryNbList();
     //Requête SQL
@@ -26,10 +64,12 @@ export default class StatsCtrl {
       });
   };
 
-  //dernier message envoyé (id)
-  static lastMsgSent = (req: Request, res: Response) => {};
-
-  //nombre de message envoyé
+  /**
+   * Nombre de message envoyé
+   *
+   * @param req
+   * @param res
+   */
   static nbMsgSent = (req: Request, res: Response) => {
     let nbMsgQuery = new StatsModel().queryNbMessageSend();
     //Requête SQL
@@ -44,11 +84,37 @@ export default class StatsCtrl {
       });
   };
 
-  //nombre de message envoyé selon le type chois
+  /**
+   * Nombre de message envoyé selon le type choisi
+   *
+   * @param req
+   * @param res
+   */
   static nbMsgSentByType = (req: Request, res: Response) => {
     let nbMsgByTypeQuery = new StatsModel().queryNbMessageByType();
     //Requête SQL
     nbMsgByTypeQuery
+      .then((response) => {
+        console.log(response);
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).send(error);
+      });
+  };
+
+  /**
+   * Dernier message envoyé (id)
+   *
+   *
+   * @param req
+   * @param res
+   */
+  static lastMsgSent = (req: Request, res: Response) => {
+    let lastMsgQuery = new StatsModel().queryLastMessage();
+    //Requête SQL
+    lastMsgQuery
       .then((response) => {
         console.log(response);
         res.status(200).send(response);
--- server/statistiques/stats.model.ts
+++ server/statistiques/stats.model.ts
@@ -1,6 +1,67 @@
 import { pool } from "../mysql/db.model";
 
 export default class StatsModel {
+  /**
+   * Requête SQL de récupération du nombre d'utilisateur regroupés par type (admin ou user)
+   *
+   * @returns Object
+   */
+  queryNbUsers = async () => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, connexion) => {
+        // When done with the connection, release it.
+        connexion.release();
+
+        if (err) throw err; // not connected!
+
+        const sql = "SELECT count(*) as nbUsers, u.typeCompte FROM user as u GROUP BY u.typeCompte;";
+        pool.query(sql, [], (error, results) => {
+          if (error) {
+            return reject({ error: true, message: error, data: [] });
+          }
+
+          if (results.affectedRows === 0) {
+            return reject({ error: true, message: "Impossible de récupérer le nombre d'utilisateurs", data: results });
+          }
+          return resolve({ error: false, message: "Nombre d'utilisateurs récupéré", data: results });
+        });
+      });
+    });
+  };
+
+  /**
+   * Requête SQL de récupération du nombre de contact
+   *
+   * @returns Object
+   */
+  queryNbContact = async () => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, connexion) => {
+        // When done with the connection, release it.
+        connexion.release();
+
+        if (err) throw err; // not connected!
+
+        const sql = "SELECT count(*) as nbList FROM contact;";
+        pool.query(sql, [], (error, results) => {
+          if (error) {
+            return reject({ error: true, message: error, data: [] });
+          }
+
+          if (results.affectedRows === 0) {
+            return reject({ error: true, message: "Impossible de récupérer le nombre de message", data: results });
+          }
+          return resolve({ error: false, message: "Messages récupéré", data: results });
+        });
+      });
+    });
+  };
+
+  /**
+   * Requête SQL de récupération du nombre de list
+   *
+   * @returns Object
+   */
   queryNbList = async () => {
     return new Promise((resolve, reject) => {
       pool.getConnection((err, connexion) => {
@@ -24,6 +85,11 @@ export default class StatsModel {
     });
   };
 
+  /**
+   * Requête SQL de récupération du nombre de messages envoyés
+   *
+   * @returns Object
+   */
   queryNbMessageSend = async () => {
     return new Promise((resolve, reject) => {
       pool.getConnection((err, connexion) => {
@@ -47,6 +113,11 @@ export default class StatsModel {
     });
   };
 
+  /**
+   * Requête SQL de récupération du nombre de messages triés par type
+   *
+   * @returns Object
+   */
   queryNbMessageByType = async () => {
     return new Promise((resolve, reject) => {
       pool.getConnection((err, connexion) => {
@@ -65,6 +136,34 @@ export default class StatsModel {
             return reject({ error: true, message: "Impossible de récupérer le nombre de message", data: results });
           }
           return resolve({ error: false, message: "Messages récupéré", data: results });
+        });
+      });
+    });
+  };
+
+  /**
+   * Requête SQL de récupération de l'ID du dernier message
+   *
+   * @returns Object
+   */
+  queryLastMessage = async () => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, connexion) => {
+        // When done with the connection, release it.
+        connexion.release();
+
+        if (err) throw err; // not connected!
+
+        const sql = "SELECT LAST_INSERT_ID() as idDernierMessage FROM message LIMIT 1;";
+        pool.query(sql, [], (error, results) => {
+          if (error) {
+            return reject({ error: true, message: error, data: [] });
+          }
+
+          if (results.affectedRows === 0) {
+            return reject({ error: true, message: "Impossible de récupérer le dernier message", data: results });
+          }
+          return resolve({ error: false, message: "Message récupéré", data: results });
         });
       });
     });
